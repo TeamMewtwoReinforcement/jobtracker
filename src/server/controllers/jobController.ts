2f33b80bcf8f5feb12ba18a88f82bf988d9b41f2
Justin Tran <justintran@Justins-MacBook-Pro-2.local>
2024-12-04 17:59:44 -0800

    jobcontroller createjob

--- src/server/controllers/jobController.ts
+++ src/server/controllers/jobController.ts
@@ -1,22 +1,59 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
+import supabase from "../db/dbconfig.ts";
 
 const jobController = {
-
   createJob: async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      company,
+      title,
+      location,
+      flexibility,
+      status,
+      dateApplied,
+      contact,
+    } = req.body;
+
     try {
-        const { company, role, location, flexbility, status, dateApplied, contact } = req.body; 
-        // TODO: add sql query to add job details to db 
-    } catch (err) {
-        const errObj = {
-            log: `Error saving job: ${err}`,
-            message: { err: "create job failed, check server log for details" },
-        };
-        return next(errObj);
+      const { data, error } = await supabase
+        .from("jobs")
+        .insert([
+          {
+            "Company Name": company,
+            Role: title,
+            "Date Applied": dateApplied,
+            "Application Status": status,
+            Location: location,
+            "Location Type": flexibility,
+            Contact: contact,
+          },
+        ])
+        .select();
+
+      // Check if there was an error
+      if (error) {
+        console.error("Supabase insert error:", error); // Log the error
+        throw new Error(error.message); // Optionally, throw an error to the next middleware
+      }
+
+      console.log("insert data", data);
+      next();
+    } catch (err: any) {
+      const errObj = {
+        log: `create job controller: ${err}`,
+        message: { err: "Error in createJob Controller" },
+      };
+      return next(errObj);
     }
-  }, 
+  },
 
   getAllJobs: async (req: Request, res: Response, next: NextFunction) => {
-    // TODO: add functionality 
+    // TODO replace this with req.body i
+    const user_id = "cd6fc064-2539-4ab6-b70a-47bbd729cac9";
+    let { data: jobs, error } = await supabase.from("jobs").select("*");
+    //.eq('id', user_id)
+    console.log("jobs in controller: ", jobs);
+    res.locals.jobs = jobs;
+    return next();
   },
 
   updateJob: async (req: Request, res: Response, next: NextFunction) => {
@@ -25,8 +62,7 @@ const jobController = {
 
   deleteJob: async (req: Request, res: Response, next: NextFunction) => {
     // TODO: add functionality
-  }
-
+  },
 };
 
 export default jobController;
